7e9d200b31e477a9abafde50a8d6ad9505ca3b85
Sharodwato Dev <102583634+Sharodwato@users.noreply.github.com>
2026-05-18 22:49:25 +0530

    Newly added hw

--- Sharodwato Loops/pn.py
+++ Sharodwato Loops/pn.py
@@ -3,9 +3,9 @@
  45
 345
 ''' 
-for i in range(1, 5):
+for i in range(1,6):
     for s in range(3-i):
         print(" ", end="")
     for j in range(6-i,6):
-        print(j, end="")
+        print(j,end="")
     print()
