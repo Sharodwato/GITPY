2c4ad64c933d92ed4b9cf6e0ebb80a965e85b8bb
Sharodwato Dev <102583634+Sharodwato@users.noreply.github.com>
2026-05-18 18:26:12 +0530

    RANDOM STUFF

---
+++ loops/pn.py
@@ -0,0 +1,14 @@
+'''
+*
+**
+***
+****
+random sh#t
+''' 
+
+for i in range (4,0,-1):
+    for s in range(i,1):
+        print("",end="")
+    for j in range(5,i,-1):
+        print("*",end="")
+    print()
