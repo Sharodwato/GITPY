93fd187c73335d360f465edadb9bb442780b2bea
Sharodwato Dev <102583634+Sharodwato@users.noreply.github.com>
2026-06-09 21:36:03 +0530

    added the docs file

---
+++ LIST + STRING CODES/las.py
@@ -0,0 +1,19 @@
+'''
+3x3 square matrix
+
+
+'''
+rows=3
+cols=3
+matrix=[]
+for i in range(rows+1):
+    row=[]
+    col=[]
+    for j in range(cols+1):
+        row.append(1)
+    matrix.append(rows)
+print("INITIALIZED MATRIX")
+for r in matrix:
+    print(r)
+            
+    
